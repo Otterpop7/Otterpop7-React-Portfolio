24445e07f5fd42f3e2c2897bbc4eb045a8e16f45
Otterpop7 <Jordanotterson7@gmail.com>
2022-05-25 16:38:44 -0600

    more complete react portfolio

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
+import { useState } from 'react';
+
 
 function App() {
+    const [selectedPage, setSelectedPage] = useState(0);
   return (
     <>
-    <Navbar/>
-    <Header/>
+    <Navbar setPage={setSelectedPage}/>
+    <Header page={selectedPage}/>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,45 +2,57 @@ import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faBars } from "@fortawesome/free-solid-svg-icons"
 
-const Navbar = () => {
+const Navbar = ({ setPage }) => {
   return (
-    <nav className="navbar navbar-expand-lg">
-        <div className="container container-fluid">
-          <a className="navbar-brand" href="#">
-            Jordan Otterson Portfolio
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-              <FontAwesomeIcon icon={faBars} style={{ color: "#e1ca96" }}/>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                
-              <li className="nav-item">
-                <a className="nav-link" href="#about-me">
-                  About Me
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#portfolio">
-                  Portfolio
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#contact">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
+    <nav className="navbar navbar-expand-lg" style={{height: "70px"}}>
+      <div className="container container-fluid">
+        <a className="navbar-brand" href="#">
+          Jordan Otterson Portfolio
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <FontAwesomeIcon icon={faBars} style={{ color: "#e1ca96" }} />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="#home" onClick={() => setPage(0)}>
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="#about-me"
+                onClick={() => setPage(1)}
+              >
+                About Me
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="#portfolio"
+                onClick={() => setPage(2)}
+              >
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#contact" onClick={() => setPage(3)}>
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
+      </div>
     </nav>
   )
 }
